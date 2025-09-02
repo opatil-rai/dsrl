8b83e209e752a742d4013b33bafa76854de162b4
Eric Rosen <erosen@theaiinstitute.com>
2025-09-02 21:37:06 +0000

    added sampling random actions in latent to eval + whether to copy action or not

--- scripts/eval_pusht_models.py
+++ scripts/eval_pusht_models.py
@@ -1,4 +1,4 @@
-from lerobot_dsrl import generate_steerable_diffpo_pusht_gym_env
+from lerobot_dsrl import generate_steerable_diffpo_pusht_gym_env, DiffpoEnvWrapper
 import numpy as np
 import imageio
 import gymnasium as gym
@@ -11,7 +11,7 @@ from typing import Optional, Dict, Any, List
 from gymnasium.wrappers import TimeLimit
 import matplotlib.pyplot as plt
 import os
-
+from copy import deepcopy
 
 class ResetOptionsWrapper(gym.Wrapper):
     def __init__(
@@ -37,30 +37,7 @@ class ResetOptionsWrapper(gym.Wrapper):
 
         return self.env.reset(**kwargs)
 
-
-if __name__ == "__main__":
-    success_threshold = 0.9
-
-    options = None
-    max_timesteps = 1000  # PushT already capped at 300 steps
-    device = "cuda"
-
-    gym_handle = "gym_pusht/PushT-v0"
-    gym_kwargs = {
-        "obs_type": "pixels_agent_pos",
-        "render_mode": "rgb_array",
-        "visualization_width": 384,
-        "visualization_height": 384,
-        "max_episode_steps": max_timesteps,
-    }
-
-    # list of seeds to evaluate
-    seeds = list(range(1, 6))  # [1,2,3,4,5]
-    env = gym.make(gym_handle, disable_env_checker=True, **gym_kwargs)
-    env.success_threshold = success_threshold
-    env = ResetOptionsWrapper(env, options=options, seeds=seeds)
-    env = TimeLimit(env, max_episode_steps=max_timesteps)
-
+def eval_base_policy(env, seeds):
     # make policy
     policy = DiffusionPolicy.from_pretrained("lerobot/diffusion_pusht").to(device)
 
@@ -90,9 +67,8 @@ if __name__ == "__main__":
 
             action = action.to("cpu").numpy().squeeze(0)
             obs, reward, terminated, truncated, info = env.step(action)
-
-            total_reward += reward
-            episode_rewards.append(total_reward)
+            reward = -1
+            episode_rewards.append(reward)
 
             frames.append(env.render())
 
@@ -102,26 +78,105 @@ if __name__ == "__main__":
         # save video
         video_path = f"videos/pusht_rollout_seed{seed}.mp4"
         imageio.mimsave(video_path, frames, fps=20)
-        print(f"Saved video for seed {seed} -> {video_path}")
+        print(f"Saved video for seed {seed} -> {video_path}, returns: {sum(episode_rewards)}")
 
-        # store results
-        all_rewards.append(episode_rewards)
-        success_flags.append(terminated and not truncated)
+       
 
-    # compute success rate
-    success_rate = np.mean(success_flags)
-    print(f"\nAverage success rate over {len(seeds)} seeds: {success_rate:.2f}")
+def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_threshold):
+    video_prefix = "random_"
+    # make diffpo
+    policy = DiffusionPolicy.from_pretrained("lerobot/diffusion_pusht").to(device)
 
-    # plot reward curves
-    plt.figure(figsize=(8, 5))
-    for i, rewards in enumerate(all_rewards):
-        plt.plot(rewards, label=f"Seed {seeds[i]}")
-    plt.xlabel("Timestep")
-    plt.ylabel("Cumulative Reward")
-    plt.title("Reward Curves per Episode")
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig("reward_curves.png")
-    plt.show()
+    # Me adding DDIM
+    policy.diffusion.noise_scheduler = DDIMScheduler(
+        num_train_timesteps=100,
+        beta_start=0.0001,
+        beta_end=0.02,
+        # beta_schedule is important
+        # this is the best we found
+        beta_schedule="squaredcos_cap_v2",
+        clip_sample=True,
+        set_alpha_to_one=True,
+        steps_offset=0,
+        prediction_type=policy.diffusion.config.prediction_type,  # or sample
+    )
 
-    env.close()
+    # Wrap the env
+    wrapped_env = DiffpoEnvWrapper(env, policy, options, seed=None, success_threshold=success_threshold,copy_first_action=copy_first_action)
+    num_rollouts = len(seeds)
+    max_frames = max_timesteps
+    all_rollout_frames = []
+
+    init_noise_generator = torch.Generator(device="cuda")
+    init_noise_generator.manual_seed(4221)
+
+    for rollout in range(num_rollouts):
+        observation, info = wrapped_env.reset()
+        total_rewards = 0
+        # get the initial frame
+        frames = wrapped_env.get_wrapper_attr("frames")
+        for current_step in range(max_frames):
+            # Action select
+            # Random action
+            # action is shape (batch_size, horizon, action dim)
+            action = wrapped_env.action_space.sample()
+
+            # Step in env in wrapper model.
+            obs, reward, terminated, truncated, info = wrapped_env.step(action)
+            total_rewards += reward
+            frames += wrapped_env.get_wrapper_attr("frames")
+
+            if terminated or truncated:
+                break
+        print(f"terminated:{terminated}, returns: {total_rewards}")
+        # get frames
+        # Save individual video
+        video_filename = f"{video_prefix}_rollout_{rollout}.mp4"
+        imageio.mimsave(video_filename, frames, fps=30)
+        print(f"Saved {video_filename}")
+
+        # Pad to max length with last frame
+        while len(frames) < max_frames:
+            frames.append(deepcopy(frames[-1]))
+
+        all_rollout_frames.append(frames)
+
+    # --- Create overlay video ---
+    overlay_frames = []
+
+    for frame_idx in range(max_frames):
+        overlay = np.mean(
+            [rollout[frame_idx] for rollout in all_rollout_frames], axis=0
+        ).astype(np.uint8)
+        overlay_frames.append(overlay)
+
+    imageio.mimsave(f"{video_prefix}_overlay.mp4", overlay_frames, fps=30)
+    print("Saved overlay.mp4")
+
+
+
+if __name__ == "__main__":
+    success_threshold = 0.85
+
+    options = None
+    max_timesteps = 1000  # PushT already capped at 300 steps
+    device = "cuda"
+
+    gym_handle = "gym_pusht/PushT-v0"
+    gym_kwargs = {
+        "obs_type": "pixels_agent_pos",
+        "render_mode": "rgb_array",
+        "visualization_width": 384,
+        "visualization_height": 384,
+        "max_episode_steps": max_timesteps,
+    }
+
+    # list of seeds to evaluate
+    seeds = list(range(1, 6))  # [1,2,3,4,5]
+    env = gym.make(gym_handle, disable_env_checker=True, **gym_kwargs)
+    env.success_threshold = success_threshold
+    env = ResetOptionsWrapper(env, options=options, seeds=seeds)
+    env = TimeLimit(env, max_episode_steps=max_timesteps)
+
+    eval_base_policy(env, seeds)
+    # eval_random_policy(env, seeds, max_timesteps, copy_first_action=False, success_threshold=success_threshold)
