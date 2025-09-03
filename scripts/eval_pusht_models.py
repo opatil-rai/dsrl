6e1edc1ca42d0958159313de287929a4edea0283
Eric Rosen <erosen@theaiinstitute.com>
2025-09-03 02:25:05 +0000

    support for evaluating sac policy

--- scripts/eval_pusht_models.py
+++ scripts/eval_pusht_models.py
@@ -7,11 +7,13 @@ import gym_pusht
 from lerobot.policies.diffusion.modeling_diffusion import DiffusionPolicy
 from lerobot.envs.utils import preprocess_observation
 import torch
+from stable_baselines3 import SAC
 from typing import Optional, Dict, Any, List
 from gymnasium.wrappers import TimeLimit
 import matplotlib.pyplot as plt
 import os
 from copy import deepcopy
+from gymnasium.wrappers import RescaleAction
 
 class ResetOptionsWrapper(gym.Wrapper):
     def __init__(
@@ -82,7 +84,7 @@ def eval_base_policy(env, seeds):
 
        
 
-def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_threshold):
+def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_threshold, sac_path, deterministic=True):
     video_prefix = "random_"
     # make diffpo
     policy = DiffusionPolicy.from_pretrained("lerobot/diffusion_pusht").to(device)
@@ -103,6 +105,14 @@ def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_thr
 
     # Wrap the env
     wrapped_env = DiffpoEnvWrapper(env, policy, options, seed=None, success_threshold=success_threshold,copy_first_action=copy_first_action)
+
+    if sac_path is not None:
+        action_min = np.ones([2])*-1
+        action_max = np.ones([2])
+        # linearlly normalize obs/action to [-1,1]
+        wrapped_env = RescaleAction(wrapped_env, min_action=action_min, max_action=action_max)
+        model = SAC.load(sac_path,env=wrapped_env)
+
     num_rollouts = len(seeds)
     max_frames = max_timesteps
     all_rollout_frames = []
@@ -111,7 +121,7 @@ def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_thr
     init_noise_generator.manual_seed(4221)
 
     for rollout in range(num_rollouts):
-        observation, info = wrapped_env.reset()
+        obs, info = wrapped_env.reset()
         total_rewards = 0
         # get the initial frame
         frames = wrapped_env.get_wrapper_attr("frames")
@@ -119,7 +129,10 @@ def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_thr
             # Action select
             # Random action
             # action is shape (batch_size, horizon, action dim)
-            action = wrapped_env.action_space.sample()
+            if sac_path is not None:
+                action = model.predict(obs, deterministic=deterministic)[0]
+            else:
+                action = wrapped_env.action_space.sample()
 
             # Step in env in wrapper model.
             obs, reward, terminated, truncated, info = wrapped_env.step(action)
@@ -156,9 +169,12 @@ def eval_random_policy(env, seeds, max_timesteps, copy_first_action, success_thr
 
 
 if __name__ == "__main__":
-    success_threshold = 0.85
+    success_threshold = 0.95
 
     options = None
+    # reset_state = np.array([314, 201, 187.21077193, 275.01629149, np.pi / 4.0])
+    # options = {"reset_to_state": reset_state}
+
     max_timesteps = 1000  # PushT already capped at 300 steps
     device = "cuda"
 
@@ -178,5 +194,9 @@ if __name__ == "__main__":
     env = ResetOptionsWrapper(env, options=options, seeds=seeds)
     env = TimeLimit(env, max_episode_steps=max_timesteps)
 
-    eval_base_policy(env, seeds)
-    # eval_random_policy(env, seeds, max_timesteps, copy_first_action=False, success_threshold=success_threshold)
+    # eval_base_policy(env, seeds)
+
+    sac_path = "./my_models/pusht_latent_new.zip"
+    copy_first_action = True
+    deterministic = True
+    eval_random_policy(env, seeds, max_timesteps, copy_first_action=copy_first_action, success_threshold=success_threshold, sac_path=sac_path,deterministic=deterministic)
